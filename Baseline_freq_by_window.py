1b0019cc4b786674d71c21bad03098b8b1d915b8
Osama Ejaz <59282708+osamaejz@users.noreply.github.com>
2023-11-15 10:37:56 +0500

    Add files via upload
    offline processing (window-based) of baseline data

---
+++ Baseline_freq_by_window.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+from scipy import signal
+
+filename = 'Post_Baseline_farihaEC.csv'
+
+save_file_name = filename[:-4] + '_processed.csv'
+
+data = pd.read_csv(filename)
+
+feedback_channel = np.array(data['8'])
+
+Fs = 500
+wL = 4 * Fs
+n = 0
+baseline_mean_frequency = []
+
+buffer_data = feedback_channel[0:2000]
+
+buffer_data = buffer_data.tolist() 
+
+for window in range(len(feedback_channel) - 2000):
+    
+    pwelch = signal.welch(buffer_data, fs=Fs, window='hanning', nperseg=wL, noverlap=wL/2, nfft=wL)
+    
+    baseline_beta = pwelch[1][int(wL*15/Fs) : int((wL*20/Fs)) + 1] # +1 due to python run till last index -1  
+    
+    baseline_mean_frequency.append(np.mean(baseline_beta))
+    
+    buffer_data.pop(0)
+    
+    buffer_data.insert(1999, feedback_channel[2000+n])
+
+    n += 1
+ 
+
+df = pd.DataFrame(baseline_mean_frequency, columns =['Mean Frequencies'])
+
+df.to_csv(save_file_name)
+
+
+
+    
